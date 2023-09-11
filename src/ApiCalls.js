0df807138ad6ed956632da911fcc6b1f89f39d33
Denzel Turambi <dturambi26@gmail.com>
2023-09-11 11:41:40 -0600

    feat: Add post request function

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -2,3 +2,13 @@ export function getData() {
   return fetch('http://localhost:3001/api/v1/reservations')
     .then(res => res.json())
 }
+
+export function postReserv(newReserv) {
+  return fetch('http://localhost:3001/api/v1/reservations', {
+    method: 'POST',
+    body: JSON.stringify(newReserv),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+}
